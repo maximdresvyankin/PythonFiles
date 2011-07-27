d17908b5316f2004cfb7effa047b9b0d8f9e6d37
Dresvyankin Maxim <tree@mdresvyankin.clc.com.ua>
2011-07-27 16:06:36 +0300

    1) Print style fixed 2) Added function for show user info

--- mail.py
+++ mail.py
@@ -5,32 +5,27 @@ import sys
 import os
 sys.path.append(os.getcwd())
 sys.path.append(os.getcwd() + "/src")
-print sys.path
-
 from mysqlhelper import *
 
 def print_hello(): 
-    print """Please select job for continue
+    print("""Please select job for continue
     1. Create new mail user
     2. Change password for existing user
-    """
+    3. Show user info
+    """)
     choice = raw_input()
     return choice
 
 def getmaneandpass():
-    print """Please enter user name 
+    print("""Please enter user name 
     (example: someusername)
     without any  @
-    """
+    """)
     usname = raw_input()
-    print """Please enter user password"""
+    print("Please enter user password")
     uspassword = raw_input()
     return (usname, uspassword)
-
-def add_modyfy_user(sql, cursor):
-    db.cursor.execute(sql)
-    db.cursor.close()
-    db.commit()
+    
 """
 Main program
 """
@@ -38,16 +33,18 @@ db = mysqlhelper('localhost', 'root', '', 'mail')
 a = str(print_hello())
 
 if (a == "1"):
-    print "You are deside to create new mail user"
+    print("You are deside to create new mail user")
     params = getmaneandpass()
-    sql="INSERT INTO virtual_users (domain_id,user,password,passplain) VALUES (2,\"%s\",PASSWORD(\"%s\"),\"%s\")" % (params[0], params[1], params[1])
-    add_modyfy_user(sql, db)
+    sql = "INSERT INTO virtual_users (domain_id,user,password,passplain) VALUES (2,\"%s\",PASSWORD(\"%s\"),\"%s\")" % (params[0], params[1], params[1])
+    db.mhsqlrun(sql)
 elif (a == "2"):
-    print "You are deside to change user password \n"
+    print("You are deside to change user password \n")
     params = getmaneandpass()
-    sql="UPDATE virtual_users SET passplain=\"%s\", password=PASSWORD(\"%s\") WHERE user=\"%s\"" % (params[1], params[1], params[0])
-    print sql;
-    add_modyfy_user(sql, db)
-
-#print sql
-#db.mhprintresult('virtual_users', '1=1')
+    sql = "UPDATE virtual_users SET passplain=\"%s\", password=PASSWORD(\"%s\") WHERE user=\"%s\"" % (params[1], params[1], params[0])
+    print(sql);
+    db.mhsqlrun(sql)
+elif (a == "3"):
+    print("Please type username without \@")
+    username = raw_input()
+    sql = "SELECT * FROM virtual_users WHERE user=\"%s\""
+    print(db.mhfetchrow("virtual_users", "user=\"%s\"" % username))
