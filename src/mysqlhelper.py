7940335a60f1bc573d43956246f6f6100fb693d1
maximdresvyankin <m.dresvyankin@gmail.com>
2011-08-01 20:52:54 +0300

    Added error processing

--- src/mysqlhelper.py
+++ src/mysqlhelper.py
@@ -5,6 +5,7 @@ Created on 9 лип. 2011
 @author: tree
 '''
 import MySQLdb
+import sys
 
 class mysqlhelper(object):
     '''
@@ -16,23 +17,36 @@ class mysqlhelper(object):
         '''
         Creating DB connection
         '''
-        self.connection = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=dbscheme)
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=dbscheme)
+            self.cursor = self.connection.cursor()
+        except MySQLdb.Error, e:
+            print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+            sys.exit (1)
+
     
     def mhfetchrow(self, dbtablename, dbwhere):
         '''
         Find one string from DB
         '''
-        sql = "SELECT * FROM %s WHERE %s" % (dbtablename, dbwhere)
-        print sql;
-        self.cursor.execute(sql)
-        result = self.cursor.fetchall()
-        return result
+        try:
+            sql = "SELECT * FROM %s WHERE %s" % (dbtablename, dbwhere)
+            print sql;
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            return result
+        except MySQLdb.Error, e:
+            print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+            sys.exit (1)
     
     def mhsqlrun (self,sql):
-        self.cursor.execute(sql)
-        self.cursor.close()
-        self.commit()
+        try:
+            self.cursor.execute(sql)
+            self.cursor.close()
+            self.commit()
+        except MySQLdb.Error, e:
+            print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+            sys.exit (1)
         
     def mhprintresult(self, dbtablename, dbwhere):
         for records in self.mhfetchrow(dbtablename, dbwhere):
@@ -40,13 +54,23 @@ class mysqlhelper(object):
                 print "%s \t" % records[i], "|",
             print "\n"
             print "#" * 80
+            
     def mhinsert(self, dbtable, dbtablerow, dbtablevalues):
-        print dbtablerow
-        print tuple(dbtablevalues)
-        sql = """
-        INSERT INTO %s (%s) VALUES %s
-        """ % (dbtable, dbtablerow, tuple(dbtablevalues))
-        print sql
-        self.cursor.execute(sql)
+        try:
+            print dbtablerow
+            print tuple(dbtablevalues)
+            sql = """
+            INSERT INTO %s (%s) VALUES %s
+            """ % (dbtable, dbtablerow, tuple(dbtablevalues))
+            print sql
+            self.cursor.execute(sql)
+        except MySQLdb.Error, e:
+            print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+            sys.exit (1)
+        
     def commit(self):
-        self.connection.commit()
+        try:
+            self.connection.commit()
+        except MySQLdb.Error, e:
+            print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+            sys.exit (1)
