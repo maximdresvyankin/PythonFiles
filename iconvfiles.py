d5df7aaab0a980b767e464d7e0123cd3a865a669
maximdresvyankin <m.dresvyankin@gmail.com>
2011-08-04 23:44:48 +0300

    1. Rewriting iconv part of programm 2. Improwed output

--- iconvfiles.py
+++ iconvfiles.py
@@ -6,26 +6,35 @@ import os.path
 import re
 
 # def findall(path):
-if (len(sys.argv)>1): path=sys.argv[1]
-else: path=raw_input("Введите директорию поиска \n")
-while (path==""):
+if (len(sys.argv) > 1): path = sys.argv[1]
+else: path = raw_input("Введите директорию поиска \n")
+while (path == ""):
     print "Введите существующую директорию поиска"
-    path=raw_input()
-    if not(os.path.exists(path)): path=""
+    path = raw_input()
+    if not(os.path.exists(path)): path = ""
     else: print "Path %s exist \n" % path
 
-print "#"*40
+print "#" * 40
+print("Start")
 #print "%s" % path
 listoffiles = []
+counter = 0
 for names in os.walk(path):
     for files in names[2]:
-        counter = 0
-        if (re.search('(.cue$)', files) and not(re.search('(.cueuft.cue)', files))): 
+        if (re.search('(cue$)', files) and not(re.search('(utf.cue)', files))): 
             listoffiles.append(os.path.join(names[0], files))
+counter = 0
+lenoflist = len(listoffiles)
 for all in listoffiles:
-     preall=all.split(".")[0]
-     print "\"%s\" \t \"%s\"" % (all, preall+".utf.cue")
-     #os.system( "iconv -f CP1251 -t UTF-8 \"%s\" > \"%s\"" % (all, all+".utf.cue"))
-    
+#     print "\"%s\" \t \"%s\"" % (all, preall + "utf.cue")
+     try:
+         os.system("iconv -f CP1251 -t UTF-8 \"%s\" > \"%s\"" % (all, all[:-3] + "utf.cue"))
+         counter+=100/lenoflist
+         print("{0:-3d} percents complete".format(counter))
+     except os.error, e:
+         print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+         sys.exit (1)
+print("Complete")
+print("#"*40)    
 #iconvpath=os.system('/usr/bin/iconv')
 #print iconvpath
