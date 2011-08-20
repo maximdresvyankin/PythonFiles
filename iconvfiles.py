a5c1f1957aca71cc635d4203214f02808b5e1fcc
maximdresvyankin <m.dresvyankin@gmail.com>
2011-08-20 15:59:14 +0300

    1. Fixed path check 2. Added open for file (check for Ubuntu NTFS bug)

--- iconvfiles.py
+++ iconvfiles.py
@@ -6,6 +6,7 @@ import re
 from chardet.universaldetector import UniversalDetector 
 
 def detectcharset(filename):
+    #Function for detect file charset
     detector = UniversalDetector()
     detector.reset()
     for line in file(filename, 'rb'):
@@ -13,19 +14,17 @@ def detectcharset(filename):
         if detector.done: break
     detector.close()
     return(detector.result)
-    
-# def findall(path):
+
+# Param checks.    
 if (len(sys.argv) > 1): path = sys.argv[1]
 else: path = raw_input("Введите директорию поиска \n")
-while (path == ""):
+while not(os.path.exists(path)):
     print "Введите существующую директорию поиска"
     path = raw_input()
-    if not(os.path.exists(path)): path = ""
-    else: print "Path %s exist \n" % path
 
+# Begin program
 print "#" * 40
 print("Start")
-#print "%s" % path
 listoffiles = []
 counter = 0
 for names in os.walk(path):
@@ -37,10 +36,17 @@ lenoflist = len(listoffiles)
 for all in listoffiles:
 #     print "\"%s\" \t \"%s\"" % (all, preall + "utf.cue")
      try:
-         inpenc=detectcharset(all)['encoding'].upper()
-         os.system("iconv -f {0} -t UTF-8 \"{1}\" > \"{2}\"".format(inpenc,all,all[:-3] + "utf.cue"))
-         counter+=100/lenoflist
+         inpenc = detectcharset(all)['encoding'].upper()
+         counter += 100 / lenoflist
          sys.stdout.write("{0:-3d}%".format(counter))
+         print all
+         try:
+             newfile = open(all[:-3] + "utf.cue", "r")
+             newfile.close()
+         except os.error, e:
+             print('Error {0} : {1}'.format(e.args[0], e.args[1]))
+             sys.exit (1)
+         os.system("iconv -f {0} -t UTF-8 \"{1}\" > \"{2}\"".format(inpenc, all, all[:-3] + "utf.cue"))
      except os.error, e:
          print('Error {0} : {1}'.format(e.args[0], e.args[1]))
          sys.exit (1)
